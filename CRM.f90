811c1f8349bbfd33fbfcfdc94d502007bc298e7e
Salvador Rodriguez-Gomez <salrodgom@upo.es>
2016-11-04 21:39:52 +0100

    modified:   CRM.f90 	modified:   input.csv

--- CRM.f90
+++ CRM.f90
@@ -43,6 +43,7 @@ module mod_CSV_io
  implicit none
  private
  public csv_file_line_count, csv_file_open_read, csv_value_count, csv_file_close_read,timestamp
+ public csv_record_append_i4
  contains
  subroutine csv_value_count ( csv_record, csv_record_status, value_count )
 !! CSV_COUNT counts the number of values in a CSV record.
@@ -65,296 +66,124 @@ module mod_CSV_io
   word_length = 0
 !  Consider each character.
   csv_char_old = ','
-
   do csv_loc = 1, csv_len
-
     csv_char = csv_record(csv_loc:csv_loc)
-!
 !  Each comma divides one value from another.
-!
     if ( csv_char_old == ',' ) then
-
       value_count = value_count + 1
       word_length = 0
-!
 !  For quotes, try using CSV_RECORD_STATUS to count the number of
 !  quoted characters.
-!
     else if ( csv_char == '"' ) then
-
       if ( 0 < csv_record_status ) then
         csv_record_status = 0
       else
         csv_record_status = csv_record_status + 1
       end if
-!
 !  Ignore blanks
-!
     else if ( csv_char == ' ' .or. csv_char == TAB ) then
-!
 !  Add character to length of word.
-!
     else
-
       word_length = word_length + 1
-
       if ( value_count == 0 ) then
         value_count = 1
       end if
-
     end if
-
     csv_char_old = csv_char
-
   end do
-
   return
 end
 subroutine csv_file_close_read ( csv_file_name, csv_file_unit )
-
-!*****************************************************************************80
-!
 !! CSV_FILE_CLOSE_READ closes a CSV file for reading.
-!
-!  Licensing:
-!
-!    This code is distributed under the GNU LGPL license.
-!
-!  Modified:
-!
-!    29 November 2008
-!
-!  Author:
-!
-!    John Burkardt
-!
-!  Parameters:
-!
 !    Input, character ( len = * ) CSV_FILE_NAME, the name of the file.
-!
 !    Input, integer ( kind = 4 ) CSV_FILE_UNIT, the unit number
-!
   implicit none
-
   character ( len = * ) csv_file_name
   integer ( kind = 4 ) csv_file_unit
-
   close ( unit = csv_file_unit )
-
   return
 end
 subroutine csv_file_close_write ( csv_file_name, csv_file_unit )
-
-!*****************************************************************************80
-!
 !! CSV_FILE_CLOSE_WRITE closes a CSV file for writing.
-!
-!  Licensing:
-!
-!    This code is distributed under the GNU LGPL license.
-!
-!  Modified:
-!
-!    22 November 2008
-!
-!  Author:
-!
-!    John Burkardt
-!
-!  Parameters:
-!
 !    Input, character ( len = * ) CSV_FILE_NAME, the name of the file.
-!
 !    Input, integer ( kind = 4 ) CSV_FILE_UNIT, the unit number
-!
   implicit none
-
   character ( len = * ) csv_file_name
   integer ( kind = 4 ) csv_file_unit
-
   close ( unit = csv_file_unit )
-
   return
 end
 subroutine csv_file_header_write ( csv_file_name, csv_file_unit, header )
-
-!*****************************************************************************80
-!
 !! CSV_FILE_HEADER_WRITE writes a header to a CSV file.
-!
-!  Licensing:
-!
-!    This code is distributed under the GNU LGPL license.
-!
-!  Modified:
-!
-!    22 November 2008
-!
-!  Author:
-!
-!    John Burkardt
-!
-!  Parameters:
-!
 !    Input, character ( len = * ) CSV_FILE_NAME, the name of the file.
-!
 !    Input, integer ( kind = 4 ) CSV_FILE_UNIT, the unit number
-!
 !    Input, character ( len = * ) HEADER, the header.
-!
   implicit none
-
   character ( len = * ) csv_file_name
   integer ( kind = 4 ) csv_file_unit
   character ( len = * ) header
-
   write ( csv_file_unit, '(a)' ) trim ( header )
-
   return
 end
 subroutine csv_file_line_count ( csv_file_name, line_num )
-
-!*****************************************************************************80
-!
 !! CSV_FILE_LINE_COUNT counts the number of lines in a CSV file.
-!
-!  Discussion:
-!
 !    This routine does not try to distinguish the possible header line,
 !    blank lines, or cases where a single CSV record extends over multiple 
 !    lines.  It simply counts the number of lines.
-!
-!  Licensing:
-!
-!    This code is distributed under the GNU LGPL license.
-!
-!  Modified:
-!
-!    22 November 2008
-!
-!  Author:
-!
-!    John Burkardt
-!
-!  Parameters:
-!
 !    Input, character ( len = * ) CSV_FILE_NAME, the name of the file.
-!
 !    Output, integer ( kind = 4 ) LINE_NUM, the number of lines.
-!
   implicit none
-
   character ( len = * ) csv_file_name
   integer ( kind = 4 ) ierror
   integer ( kind = 4 ) input_status
   integer ( kind = 4 ) input_unit
   character ( len = 1023 ) line
   integer ( kind = 4 ) line_num
-
   line_num = -1
-
   call get_unit ( input_unit )
-
   open ( unit = input_unit, file = csv_file_name, status = 'old', &
     iostat = input_status )
-
   if ( input_status /= 0 ) then
     write ( *, '(a)' ) ' '
     write ( *, '(a)' ) 'CSV_FILE_LINE_COUNT - Fatal error!'
     write ( *, '(a,i8)' ) '  Could not open "' // trim ( csv_file_name ) // '".'
     stop
   end if
-
   line_num = 0
-
   do
-
     read ( input_unit, '(a)', iostat = input_status ) line
-
     if ( input_status /= 0 ) then
       ierror = line_num
       exit
     end if
-
     line_num = line_num + 1
-
   end do
-
   close ( unit = input_unit )
-
   return
 end
 subroutine csv_file_record_write ( csv_file_name, csv_file_unit, record )
-
-!*****************************************************************************80
-!
 !! CSV_FILE_RECORD_WRITE writes a record to a CSV file.
-!
-!  Licensing:
-!
-!    This code is distributed under the GNU LGPL license.
-!
-!  Modified:
-!
-!    22 November 2008
-!
-!  Author:
-!
-!    John Burkardt
-!
-!  Parameters:
-!
 !    Input, character ( len = * ) CSV_FILE_NAME, the name of the file.
-!
 !    Input, integer ( kind = 4 ) CSV_FILE_UNIT, the unit number
-!
 !    Input, character ( len = * ) RECORD, the record.
-!
   implicit none
-
   character ( len = * ) csv_file_name
   integer ( kind = 4 ) csv_file_unit
   character ( len = * ) record
-
   write ( csv_file_unit, '(a)' ) trim ( record )
-
   return
 end
 subroutine csv_file_open_read ( csv_file_name, csv_file_unit )
-
-!*****************************************************************************80
-!
 !! CSV_FILE_OPEN_READ opens a CSV file for reading.
-!
-!  Licensing:
-!
-!    This code is distributed under the GNU LGPL license.
-!
-!  Modified:
-!
-!    29 November 2008
-!
-!  Author:
-!
-!    John Burkardt
-!
-!  Parameters:
-!
 !    Input, character ( len = * ) CSV_FILE_NAME, the name of the file.
-!
 !    Output, integer ( kind = 4 ) CSV_FILE_UNIT, the unit number
-!
   implicit none
-
   character ( len = * ) csv_file_name
   integer ( kind = 4 ) csv_file_status
   integer ( kind = 4 ) csv_file_unit
-
   call get_unit ( csv_file_unit )
-
   open ( unit = csv_file_unit, file = csv_file_name, status = 'old', &
     iostat = csv_file_status )
-
   if ( csv_file_status /= 0 ) then
     write ( *, '(a)' ) ' '
     write ( *, '(a)' ) 'CSV_FILE_OPEN_READ - Fatal error!'
@@ -362,156 +191,69 @@ subroutine csv_file_open_read ( csv_file_name, csv_file_unit )
     csv_file_unit = - 1
     stop
   end if
-
   return
 end
 subroutine csv_file_open_write ( csv_file_name, csv_file_unit )
-
-!*****************************************************************************80
-!
 !! CSV_FILE_OPEN_WRITE opens a CSV file for writing.
-!
-!  Licensing:
-!
-!    This code is distributed under the GNU LGPL license.
-!
-!  Modified:
-!
-!    22 November 2008
-!
-!  Author:
-!
-!    John Burkardt
-!
-!  Parameters:
-!
 !    Input, character ( len = * ) CSV_FILE_NAME, the name of the file.
-!
 !    Output, integer ( kind = 4 ) CSV_FILE_UNIT, the unit number
-!
   implicit none
-
   character ( len = * ) csv_file_name
   integer ( kind = 4 ) csv_file_status
   integer ( kind = 4 ) csv_file_unit
-
   call get_unit ( csv_file_unit )
-
   open ( unit = csv_file_unit, file = csv_file_name, status = 'replace', &
     iostat = csv_file_status )
-
   if ( csv_file_status /= 0 ) then
     write ( *, '(a)' ) ' '
     write ( *, '(a)' ) 'CSV_FILE_OPEN_WRITE - Fatal error!'
     write ( *, '(a,i8)' ) '  Could not open "' // trim ( csv_file_name ) // '".'
     stop
   end if
-
   return
 end
 subroutine csv_record_append_i4 ( i4, record )
-
-!*****************************************************************************80
-!
 !! CSV_RECORD_APPEND_I4 appends an I4 to a CSV record.
-!
-!  Licensing:
-!
-!    This code is distributed under the GNU LGPL license.
-!
-!  Modified:
-!
-!    24 November 2008
-!
-!  Author:
-!
-!    John Burkardt
-!
-!  Parameters:
-!
 !    Input, integer ( kind = 4 ) I4, the integer to be appended
-!
 !    Input/output, character ( len = * ) RECORD, the CSV record.
-!
   implicit none
-
   character ( len = 5 ) fmat
   integer ( kind = 4 ) i
   integer ( kind = 4 ) i4
   integer ( kind = 4 ) i4_len
-  !integer ( kind = 4 ) i4_width
   character ( len = * ) record
-!
-!  Locate last used location in RECORD.
-!
   i = len_trim ( record )
-!
 !  Append comma.
-!
   if ( 0 < i ) then
     i = i + 1
     record(i:i) = ','
   end if
-!
 !  Determine "width" of I4.
-!
   i4_len = i4_width ( i4 )
-!
 !  Create format for I4.
-!
   write ( fmat, '(a,i2,a)' ) '(i', i4_len, ')'
-!
 !  Write I4 to RECORD.
-!
   write ( record(i+1:i+i4_len), fmat ) i4
-
   return
 end
 subroutine csv_record_append_r4 ( r4, record )
-
-!*****************************************************************************80
-!
 !! CSV_RECORD_APPEND_R4 appends an R4 to a CSV record.
-!
-!  Licensing:
-!
-!    This code is distributed under the GNU LGPL license.
-!
-!  Modified:
-!
-!    29 November 2008
-!
-!  Author:
-!
-!    John Burkardt
-!
-!  Parameters:
-!
 !    Input, real ( kind = 8 ) R4, the value to be appended
-!
 !    Input/output, character ( len = * ) RECORD, the CSV record.
-!
   implicit none
-
   character ( len = 5 ) fmat
   integer ( kind = 4 ) i
   integer ( kind = 4 ) i4
   integer ( kind = 4 ) i4_len
-  !integer ( kind = 4 ) i4_width
   real ( kind = 4 ) r4
   character ( len = * ) record
-!
 !  Locate last used location in RECORD.
-!
   i = len_trim ( record )
-!
 !  Append comma.
-!
   if ( 0 < i ) then
     i = i + 1
     record(i:i) = ','
   end if
-
   if ( r4 == 0.0E+00 ) then
     i = i + 1
     record(i:i) = '0'
@@ -523,35 +265,13 @@ subroutine csv_record_append_r4 ( r4, record )
   else
     write ( record(i+1:i+14), '(g14.6)' ) r4
   end if
-
   return
 end
 subroutine csv_record_append_r8 ( r8, record )
-
-!*****************************************************************************80
-!
 !! CSV_RECORD_APPEND_R8 appends an R8 to a CSV record.
-!
-!  Licensing:
-!
-!    This code is distributed under the GNU LGPL license.
-!
-!  Modified:
-!
-!    29 November 2008
-!
-!  Author:
-!
-!    John Burkardt
-!
-!  Parameters:
-!
 !    Input, real ( kind = 8 ) R8, the value to be appended
-!
 !    Input/output, character ( len = * ) RECORD, the CSV record.
-!
   implicit none
-
   character ( len = 5 ) fmat
   integer ( kind = 4 ) i
   integer ( kind = 4 ) i4
@@ -559,18 +279,13 @@ subroutine csv_record_append_r8 ( r8, record )
   !integer ( kind = 4 ) i4_width
   real ( kind = 8 ) r8
   character ( len = * ) record
-!
 !  Locate last used location in RECORD.
-!
   i = len_trim ( record )
-!
 !  Append comma.
-!
   if ( 0 < i ) then
     i = i + 1
     record(i:i) = ','
   end if
-
   if ( r8 == 0.0D+00 ) then
     i = i + 1
     record(i:i) = '0'
@@ -582,147 +297,62 @@ subroutine csv_record_append_r8 ( r8, record )
   else
     write ( record(i+1:i+14), '(g14.6)' ) r8
   end if
-
   return
 end
 subroutine csv_record_append_s ( s, record )
-
-!*****************************************************************************80
-!
 !! CSV_RECORD_APPEND_S appends a string to a CSV record.
-!
-!  Licensing:
-!
-!    This code is distributed under the GNU LGPL license.
-!
-!  Modified:
-!
-!    26 November 2008
-!
-!  Author:
-!
-!    John Burkardt
-!
-!  Parameters:
-!
 !    Input, character ( len = * ) S, the string to be appended
-!
 !    Input/output, character ( len = * ) RECORD, the CSV record.
-!
   implicit none
-
   integer ( kind = 4 ) i
   character ( len = * ) record
   character ( len = * ) s
   integer ( kind = 4 ) s_len
-!
 !  Locate last used location in RECORD.
-!
   i = len_trim ( record )
-!
 !  Append a comma.
-!
   if ( 0 < i ) then
     i = i + 1
     record(i:i) = ','
   end if
-!
 !  Prepend a quote.
-!
   i = i + 1
   record(I:i) = '"'
-!
 !  Write S to RECORD.
-!
   s_len = len_trim ( s )
   record(i+1:i+s_len) = s(1:s_len)
   i = i + s_len
-!
 !  Postpend a quote
-!
   i = i + 1
   record(i:i) = '"'
-
   return
 end
 subroutine get_unit ( iunit )
-
-!*****************************************************************************80
-!
 !! GET_UNIT returns a free FORTRAN unit number.
-!
-!  Discussion:
-!
-!    A "free" FORTRAN unit number is an integer between 1 and 99 which
-!    is not currently associated with an I/O device.  A free FORTRAN unit
-!    number is needed in order to open a file with the OPEN command.
-!
-!    If IUNIT = 0, then no free FORTRAN unit could be found, although
-!    all 99 units were checked (except for units 5, 6 and 9, which
-!    are commonly reserved for console I/O).
-!
-!    Otherwise, IUNIT is an integer between 1 and 99, representing a
-!    free FORTRAN unit.  Note that GET_UNIT assumes that units 5 and 6
-!    are special, and will never return those values.
-!
-!  Licensing:
-!
-!    This code is distributed under the GNU LGPL license.
-!
-!  Modified:
-!
-!    26 October 2008
-!
-!  Author:
-!
-!    John Burkardt
-!
-!  Parameters:
-!
-!    Output, integer ( kind = 4 ) IUNIT, the free unit number.
-!
   implicit none
-
   integer ( kind = 4 ) i
   integer ( kind = 4 ) ios
   integer ( kind = 4 ) iunit
   logical lopen
-
   iunit = 0
-
   do i = 1, 99
-
     if ( i /= 5 .and. i /= 6 .and. i /= 9 ) then
-
       inquire ( unit = i, opened = lopen, iostat = ios )
-
       if ( ios == 0 ) then
         if ( .not. lopen ) then
           iunit = i
           return
         end if
       end if
-
     end if
-
   end do
-
   return
 end
 integer function i4_log_10 ( i )
-
-!*****************************************************************************80
-!
 !! I4_LOG_10 returns the integer part of the logarithm base 10 of an I4.
-!
-!  Discussion:
-!
 !    I4_LOG_10 ( I ) + 1 is the number of decimal digits in I.
-!
 !    An I4 is an integer ( kind = 4 ) value.
-!
 !  Example:
-!
 !        I  I4_LOG_10
 !    -----  --------
 !        0    0
@@ -736,74 +366,36 @@ integer function i4_log_10 ( i )
 !      101    2
 !      999    2
 !     1000    3
-!     1001    3
 !     9999    3
 !    10000    4
-!
-!  Licensing:
-!
-!    This code is distributed under the GNU LGPL license.
-!
-!  Modified:
-!
-!    08 June 2003
-!
-!  Author:
-!
-!    John Burkardt
-!
-!  Parameters:
-!
 !    Input, integer ( kind = 4 ) I, the number whose logarithm base 10
 !    is desired.
-!
 !    Output, integer ( kind = 4 ) I4_LOG_10, the integer part of the
 !    logarithm base 10 of the absolute value of X.
-!
   implicit none
-
   integer ( kind = 4 ) i
   integer ( kind = 4 ) i_abs
-  !integer ( kind = 4 ) i4_log_10
   integer ( kind = 4 ) ten_pow
-
   if ( i == 0 ) then
-
     i4_log_10 = 0
-
   else
-
     i4_log_10 = 0
     ten_pow = 10
-
     i_abs = abs ( i )
-
     do while ( ten_pow <= i_abs )
       i4_log_10 = i4_log_10 + 1
       ten_pow = ten_pow * 10
     end do
-
   end if
-
   return
 end
 integer function i4_width ( i )
-
-!*****************************************************************************80
-!
 !! I4_WIDTH returns the "width" of an I4.
-!
-!  Discussion:
-!
 !    The width of an integer is the number of characters necessary to print it.
-!
 !    The width of an integer can be useful when setting the appropriate output
 !    format for a vector or array of values.
-!
 !    An I4 is an integer ( kind = 4 ) value.
-!
 !  Example:
-!
 !        I  I4_WIDTH
 !    -----  -------
 !    -1234    5
@@ -816,29 +408,11 @@ integer function i4_width ( i )
 !      123    3
 !     1234    4
 !    12345    5
-!
-!  Licensing:
-!
-!    This code is distributed under the GNU LGPL license.
-!
-!  Modified:
-!
-!    04 December 2004
-!
-!  Author:
-!
-!    John Burkardt
-!
-!  Parameters:
-!
 !    Input, integer ( kind = 4 ) I, the number whose width is desired.
-!
 !    Output, integer ( kind = 4 ) I4_WIDTH, the number of characters
 !    necessary to represent the integer in base 10, including a negative
 !    sign if necessary.
-!
   implicit none
-
   integer ( kind = 4 ) i
   if ( 0 < i ) then
     i4_width = i4_log_10 ( i ) + 1
@@ -847,37 +421,12 @@ integer function i4_width ( i )
   else if ( i < 0 ) then
     i4_width = i4_log_10 ( i ) + 2
   end if
-
   return
 end
 subroutine timestamp ( )
-
-!*****************************************************************************80
-!
 !! TIMESTAMP prints the current YMDHMS date as a time stamp.
-!
-!  Example:
-!
 !    31 May 2001   9:45:54.872 AM
-!
-!  Licensing:
-!
-!    This code is distributed under the GNU LGPL license.
-!
-!  Modified:
-!
-!    18 May 2013
-!
-!  Author:
-!
-!    John Burkardt
-!
-!  Parameters:
-!
-!    None
-!
   implicit none
-
   character ( len = 8 ) ampm
   integer ( kind = 4 ) d
   integer ( kind = 4 ) h
@@ -891,9 +440,7 @@ subroutine timestamp ( )
   integer ( kind = 4 ) s
   integer ( kind = 4 ) values(8)
   integer ( kind = 4 ) y
-
   call date_and_time ( values = values )
-
   y = values(1)
   m = values(2)
   d = values(3)
@@ -901,7 +448,6 @@ subroutine timestamp ( )
   n = values(6)
   s = values(7)
   mm = values(8)
-
   if ( h < 12 ) then
     ampm = 'AM'
   else if ( h == 12 ) then
@@ -922,10 +468,8 @@ subroutine timestamp ( )
       end if
     end if
   end if
-
   write ( *, '(i2,1x,a,1x,i4,2x,i2,a1,i2.2,a1,i2.2,a1,i3.3,1x,a)' ) &
     d, trim ( month(m) ), y, h, ':', n, ':', s, '.', mm, trim ( ampm )
-
   return
 end
 end module
@@ -1014,7 +558,7 @@ module mod_input_output
  ! variable
  integer                    :: seed
  logical                    :: stop_flag = .false.
- logical                    :: seed_flag = .false.
+ logical                    :: seed_flag = .true.
  contains
 !
  subroutine read_input()
@@ -1030,24 +574,32 @@ module mod_input_output
 !
  subroutine ReadCSV()
   use mod_csv_io
+  use mod_random
   implicit none
   character ( len = 80 ) :: csv_file_name = 'input.csv'
   integer   ( kind = 4 ) :: csv_file_status
   integer   ( kind = 4 ) :: csv_file_unit
   integer   ( kind = 4 ) :: csv_record_status
-  integer   ( kind = 4 ) :: i
+  integer   ( kind = 4 ) :: i,k = 0
   integer   ( kind = 4 ) :: line_num
   character ( len = 120 ):: record
+  character ( len = 120 ):: string 
   integer   ( kind = 4 ) :: value_count
   !csv_file_line_count, csv_file_open_read, csv_value_count, csv_file_close_read
   call csv_file_line_count ( csv_file_name, line_num )
   write ( *, '(a,i8,a)' ) '  File contains ', line_num, ' lines.'
   call csv_file_open_read ( csv_file_name, csv_file_unit )
-  do i = 1, line_num
+  read ( csv_file_unit, '(a)', iostat = csv_file_status ) record
+  write(6,'(i5,2x,a)')1,trim(record)
+  do i = 2, line_num
     read ( csv_file_unit, '(a)', iostat = csv_file_status ) record
-    !write ( 6, '(a)' ) i, trim ( record )
-    write(6,'(i5,2x,a)')i,record
     call csv_value_count ( record, csv_record_status, value_count )
+    if(value_count>=k) k = value_count
+    !write ( 6, '(a)' ) i, trim ( record )
+    string=record
+    !write(6,'(i5,2x,a)')i,record
+    call csv_record_append_i4(randint(1,10,seed),string)
+    write(6,'(i5,2x,a)')i,trim(string)
     write ( 6, * ) i, value_count
   end do
   call csv_file_close_read ( csv_file_name, csv_file_unit )
@@ -1065,12 +617,11 @@ program CRM
   call read_input()
   if (seed_flag) then
    call init_random_seed(seed)
+   write(6,'(a,1x,i20)')'Random Seed',seed
    seed_flag=.false.
   end if
   if(stop_flag) STOP 'CRM STOPPED'
   call ReadCSV()
-  !write(6,'(a)')'Estoy esperando clientes'
-  !call write_output()
  !end do eterno_viajero
  call timestamp()
 end program CRM
